d424d97308bcc5c854ce95d354ae0100584dd6a4
Priscila Costa <prcwebrj@icloud.com>
2017-05-26 14:31:21 -0300

    Terceiro commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task("compilar-css", function(){
-	return gulp.src("./source/sass/*.scss")
+	return gulp.src("./source/scss/*.scss")
 		.pipe(sass())
 		.pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task("mova", function(){
-	return gulp.src("./dist/css/style.css")
-	.pipe(gulp.dest("./outro/css"));
-})
+gulp.task("minificar-html", function(){
+	return gulp.src("./source/*.html")
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest("./dist"));
+});
 
 gulp.task("escuta-me", function(){
 	gulp.watch("./source/sass/*.scss", ["compilar-css"]);
